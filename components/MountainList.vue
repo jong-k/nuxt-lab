ac55e309d67b9f54cbc1343e7497820638dabef9
jong-k <fully.charged07@gmail.com>
2024-08-23 00:36:28 +0900

    feat: 자식 컴포넌트에서 event emit 후 부모 컴포넌트에서 props로 내리기

--- components/MountainList.vue
+++ components/MountainList.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="w-full bg-amber-200">마운틴 리스트</div>
+  <div class="w-full bg-amber-200">현재 산: {{ mountain }}</div>
 </template>
+
+<script setup lang="ts">
+const { mountain } = defineProps({
+  mountain: String,
+});
+</script>
--- pages/mountain.vue
+++ pages/mountain.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="px-4 flex flex-col gap-4">
     <MountainHeader @change-mountain="(args) => (mountainName = args)" />
-    <MountainList />
+    <MountainList :mountain="mountainName" />
   </div>
 </template>
 
 <script setup lang="ts">
 const mountainName = ref("");
-
-watch(mountainName, (newMountain) => {
-  console.log(newMountain);
-});
 </script>
