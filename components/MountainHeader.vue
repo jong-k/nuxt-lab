1e7c91bb84bd4ef74d13419dd35dab08f90215ef
jong-k <fully.charged07@gmail.com>
2024-08-22 23:43:24 +0900

    feat: input menu 데이터 변경 시 산 이름 출력

--- components/MountainHeader.vue
+++ components/MountainHeader.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="w-full bg-fuchsia-200 flex items-center justify-between py-4">
     <h2>마운틴 헤더</h2>
-    <UInputMenu />
+    <h2>현재 selectedMountain: {{ selectedMountain }}</h2>
+    <UInputMenu v-model="selectedMountain" :options="mountainList" />
   </div>
 </template>
+
+<script setup lang="ts">
+const mountainList = ["북한산", "관악산", "아차산"];
+const selectedMountain = ref(mountainList[0]);
+const emit = defineEmits(["changeMountain"]);
+
+onMounted(() => {
+  console.log(selectedMountain.value);
+});
+
+watch(selectedMountain, (newSelectedMountain) => {
+  console.log(newSelectedMountain);
+});
+</script>
