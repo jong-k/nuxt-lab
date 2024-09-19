8f83712ec94b77925e2cc13a4e1a4247313ec986
jong-k <fully.charged07@gmail.com>
2024-09-19 23:44:05 +0900

    feat: UTabs 컴포넌트 커스텀 추가

---
+++ pages/tabs/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>UTabs Test</h2>
+    <div>
+      <UTabs
+        :ui="{
+          list: { background: 'bg-blue-500', marker: { background: 'bg-yellow-500' } },
+        }"
+        :items="items"
+      >
+        <template #account="{ item }">
+          <div>{{ item.label }}</div>
+        </template>
+
+        <template #password="{ item }">
+          <div>{{ item.label }}</div>
+        </template>
+      </UTabs>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const items = [
+  {
+    slot: "account",
+    label: "Account",
+  },
+  {
+    slot: "password",
+    label: "Password",
+  },
+];
+</script>
