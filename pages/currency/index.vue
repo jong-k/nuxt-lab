fa6c55545ca42648f0cafa74cc5b3fbcb55ef84b
jong-k <fully.charged07@gmail.com>
2024-10-25 15:39:25 +0900

    feat: 인덱스 표시

--- pages/currency/index.vue
+++ pages/currency/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <div>헤더</div>
+  <div class="flex flex-col gap-4">
+    <div class="w-full p-2 bg-gradient-to-r from-red-100 to-red-500">
+      <div>
+        take: {{ takeRef }}
+        <URange color="teal" :min="5" :max="95" :step="5" v-model="takeRef" />
+      </div>
+    </div>
     <div>
-      <div class="text-red-400" v-for="currency in data" :key="currency.code">
+      <div class="flex items-center" v-for="(currency, idx) in data" :key="currency.code">
+        <div class="text-2xl text-teal-400">{{ idx }}</div>
         <pre>{{ currency }}</pre>
       </div>
     </div>
@@ -10,12 +16,19 @@
 </template>
 
 <script setup lang="ts">
-import { Currency } from "@faker-js/faker";
-const { data } = await useAsyncData("currency", () =>
-  $fetch("/api/currency", {
-    query: {
-      size: 100,
-    },
-  }),
+const takeRef = ref(20);
+const debouncedTakeRef = refDebounced(takeRef, 400);
+
+const { data } = await useAsyncData(
+  "currency",
+  () =>
+    $fetch("/api/currency", {
+      query: {
+        take: debouncedTakeRef.value,
+      },
+    }),
+  {
+    watch: [debouncedTakeRef],
+  },
 );
 </script>
